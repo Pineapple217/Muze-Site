2b6bf5b5c7ce18600c3489de78e8bd8f35d67226
Jules Rosier <55190371+Pineapple217@users.noreply.github.com>
2022-08-31 16:03:07 +0200

    pw mail fix

--- django_app/MuzeSite/settings.py
+++ django_app/MuzeSite/settings.py
@@ -30,9 +30,11 @@ DEPLOY = os.getenv('DEPLOY')
 if not DEPLOY:
     SECRET_KEY = 'django-insecure-)(6i&+lg77#345nu(y4+0i9!gaam2w^ni3r2p-w&_b39b#r_vm'
     DEBUG = os.getenv('DEBUG')
+    HOST_URL = os.getenv('HOST_URL')
 else:
     SECRET_KEY = os.getenv('SECRET_KEY')
     DEBUG = True
+    HOST_URL = '127.0.0.1:8000'
 
 
 ALLOWED_HOSTS = ['*']
--- django_app/leden/views.py
+++ django_app/leden/views.py
@@ -150,7 +150,7 @@ def password_reset_request(request):
                     email_template_name = "registration/password/password_reset_email.html"
                     mail_context = {
                     "email":user.email,
-                    'domain': request.get_host(),
+                    'domain': settings.HOST_URL,
                     'site_name': 'Jeughuis De Muze',
                     "uid": urlsafe_base64_encode(force_bytes(user.pk)),
                     "user": user,
