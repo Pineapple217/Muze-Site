1ecbe9ce22df4f32c264a7cd896ee13c9431b7bc
Jules Rosier <55190371+Pineapple217@users.noreply.github.com>
2022-09-27 19:44:35 +0200

    zonder template

--- docker/certbot/certify-init.sh
+++ docker/certbot/certify-init.sh
@@ -7,7 +7,7 @@ set -e
 # Use netcat (nc) to check port 80, and keep checking every 5 seconds
 # until it is available. This is so nginx has time to start before
 # certbot runs.
-until nc -z proxy 80; do
+until nc -z nginx 80; do
     echo "Waiting for proxy..."
     sleep 5s & wait ${!}
 done
--- docker/nginx/run.sh
+++ docker/nginx/run.sh
@@ -3,9 +3,9 @@
 set -e
 
 echo "Checking for dhparams.pem"
-if [ ! -f "/vol/proxy/ssl-dhparams.pem" ]; then
+if [ ! -f "/proxy/ssl-dhparams.pem" ]; then
   echo "dhparams.pem does not exist - creating it"
-  openssl dhparam -out /vol/proxy/ssl-dhparams.pem 2048
+  openssl dhparam -out /proxy/ssl-dhparams.pem 2048
 fi
 
 # Avoid replacing these with envsubst
