72ac4fff202a0bcde626d0e499439d6ea3decd27
Pineapple217 <rosierjules@gmail.com>
2023-04-05 10:19:48 +0200

    bestuur view test

--- django_app/main/views.py
+++ django_app/main/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 import logging
 
+
 def home(request):
-    return render(request, 'main/home.html')
+    return render(request, "main/home.html")
+
+
+def bestuur(request):
+    context = {}
+    context["rvb"] = User.objects.filter(groups__name="Raad van bestuur")
+    context["stuurgroep"] = User.objects.filter(groups__name="Bestuurslid")
+    return render(request, "main/bestuur.html", context=context)
