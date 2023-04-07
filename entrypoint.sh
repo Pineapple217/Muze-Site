61e12e2085d0658a82b691dda95e10c0bd6ce424
Pineapple217 <rosierjules@gmail.com>
2023-04-07 22:04:24 +0200

    test2

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ python manage.py collectstatic --no-input
 
 python manage.py runapscheduler &
 
-gunicorn MuzeSite.wsgi:application --bind 0.0.0.0:8000 --log-file=- --capture-output --log-level info
+gunicorn MuzeSite.wsgi:application --bind 0.0.0.0:8000 --log-file=- --capture-output --log-level info --access-logfile -
